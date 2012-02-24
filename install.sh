f31a21e10a18c9da52a427a5be97f2e9a5c5801c
David Elentok <3david@gmail.com>
2012-02-24 16:42:49 +0200

    renamed vimrc to .vimrc (to be compatible with the Vim dir in windows)

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ DIR=$(cd -P $DIR && pwd)
 echo "=============================="
 echo "Installing ~/.vim and ~/.vimrc symlinks"
 echo "=============================="
-ln -sf "$DIR/vimrc" ~/.vimrc
+ln -sf "$DIR/.vimrc" ~/.vimrc
 ln -sf "$DIR/vimfiles" ~/.vim
 
 bash "$DIR/vimfiles/bundle/powerline/fonts/install.sh"
